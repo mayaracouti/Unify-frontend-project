2bde50396837891ae111396825542e23e5474c3e
mayara <mayaraferreiracout@example.com>
2026-04-12 02:42:27 -0300

    configurações de rota

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
-import { Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-red-500 text-xl">
-        Funcionando 🚀
-      </Text>
-    </View>
-  );
-}
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
+};
